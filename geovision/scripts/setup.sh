b87677021da683423d81e930a3bb62b432689e89
Sambhav Chandra <vader.sam10@gmail.com>
2025-09-06 18:16:07 +0530

    updated setup.sh

--- geovision/scripts/setup.sh
+++ geovision/scripts/setup.sh
@@ -5,12 +5,19 @@ apt -y install btop neovim rsync fzf ripgrep bat fish
 echo "cleaning up"
 apt -y autoremove && apt -y autoclean
 
-echo "creating project directories"
-mkdir ~/dev ~/datasets ~/experiments ~/models
-cd ~/dev && git clone https://github.com/citruscaesar/geovision.git 
-
 echo "installing pixi"
 curl -fsSL https://pixi.sh/install.sh | bash 
+
+echo "updating ~/.bashrc"
 echo "alias bat=batcat" >> ~/.bashrc
+echo "alias btop=btop --utf-force" >> ~/.bashrc
 echo 'if [ -f ~/.bashrc ]; then source ~/.bashrc; fi' >> ~/.bash_profile
 source ~/.bashrc
+
+echo "creating project directories"
+mkdir ~/dev ~/datasets ~/experiments ~/models
+
+echo "cloning geovision and installing required packages"
+cd ~/dev && git clone https://github.com/citruscaesar/geovision.git 
+cd ~/dev/geovision
+pixi install -e dev
